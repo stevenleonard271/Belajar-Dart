b78b556ab350939998168251ba5e88ce7ec1be3b
stevenleonard271 <stevenleonard271@gmail.com>
2021-01-28 08:27:55 +0700

    init

--- bin/belajar_oop2.dart
+++ bin/belajar_oop2.dart
@@ -5,58 +5,52 @@ import 'package:belajar_oop2/belajar_oop2.dart' as belajar_oop2;
 void main(List<String> arguments) {
   Segitiga segitigasatu;
   double luasstg, alasstg, tinggistg;
+  String repeat;
 
 // Segitiga
 
-  print('================');
-  print('1. Luas Segitiga');
-  print('2. Luas Persegi Panjang');
-  print('================');
+  Menu();
 
   int angkapilihan;
 
   print('Masukkan angka pilihan anda');
   angkapilihan = int.tryParse(stdin.readLineSync());
-  angkapilihan.toString();
 
-  switch (angkapilihan) {
-    case 1: //pembuatan segitiga
-      segitigasatu = Segitiga();
+  // print('Masukkan angka pilihan anda');
+  // angkapilihan = int.tryParse(stdin.readLineSync());
 
-      print('Masukkan alas segitiga');
-      segitigasatu.alas = double.tryParse(stdin.readLineSync());
-      alasstg = segitigasatu.alas;
-      print('Masukkan tinggi segitiga');
-      segitigasatu.tinggi = double.tryParse(stdin.readLineSync());
-      tinggistg = segitigasatu.tinggi;
-      
-      luasstg = segitigasatu.luas(alasstg, tinggistg);
-      luasstg.toInt().toString();
-      print('Luas segitiga adalah '+luasstg.toString()+' cm');
-      break;
+  if (angkapilihan == 1) {
+    segitigasatu = Segitiga();
 
-    case 2:
-// Persegi panjang
-      Persegipanjang persegipanjangsatu;
-      double luaspp, panjangpp, lebarpp;
+    print('Masukkan alas segitiga');
+    segitigasatu.alas = double.tryParse(stdin.readLineSync());
+    alasstg = segitigasatu.alas;
+    print('Masukkan tinggi segitiga');
+    segitigasatu.tinggi = double.tryParse(stdin.readLineSync());
+    tinggistg = segitigasatu.tinggi;
 
-      persegipanjangsatu = Persegipanjang();
-      print('Masukkan panjang persegi panjang');
-      persegipanjangsatu.panjang = double.tryParse(stdin.readLineSync());
-      panjangpp = persegipanjangsatu.panjang;
-      print('Masukkan lebar persegi panjang');
-      persegipanjangsatu.lebar = double.tryParse(stdin.readLineSync());
-      lebarpp = persegipanjangsatu.lebar;
+    luasstg = segitigasatu.luas(alasstg, tinggistg);
+    luasstg.toInt().toString();
+    print('Luas segitiga adalah ' + luasstg.toString() + ' cm');
+  } else if (angkapilihan == 2) {
+    Persegipanjang persegipanjangsatu;
+    double luaspp, panjangpp, lebarpp;
 
-      luaspp = persegipanjangsatu.luas(panjangpp, lebarpp);
-      luaspp.toInt().toString();
+    persegipanjangsatu = Persegipanjang();
+    print('Masukkan panjang persegi panjang');
+    persegipanjangsatu.panjang = double.tryParse(stdin.readLineSync());
+    panjangpp = persegipanjangsatu.panjang;
+    print('Masukkan lebar persegi panjang');
+    persegipanjangsatu.lebar = double.tryParse(stdin.readLineSync());
+    lebarpp = persegipanjangsatu.lebar;
 
-      print('Luas persegi panjang adalah'+luaspp.toString()+' cm');
-      break;
+    luaspp = persegipanjangsatu.luas(panjangpp, lebarpp);
+    luaspp.toInt().toString();
 
-
+    print('Luas persegi panjang adalah ' + luaspp.toString() + ' cm');
+  } else {
+    print('Nomor yang anda masukkan salah');
   }
-  ;
 }
 
 class Segitiga {
@@ -67,4 +61,11 @@ class Segitiga {
 class Persegipanjang {
   double panjang, lebar;
   double luas(panjang, lebar) => this.panjang * lebar;
+}
+
+void Menu() {
+  print('============================');
+  print('|1. Luas Segitiga          |');
+  print('|2. Luas Persegi Panjang   |');
+  print('============================');
 }
---
+++ test/belajar_oop2_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_oop2/belajar_oop2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
